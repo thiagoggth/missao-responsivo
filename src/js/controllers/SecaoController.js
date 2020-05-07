f9546fe1dc30a07602cb5acc2c6b899b02517431
Thiago Augusto <thiagoggth@gmail.com>
2020-05-07 17:15:48 -0400

    Used ApiFactory to make a list of all secoes

--- src/js/controllers/SecaoController.js
+++ src/js/controllers/SecaoController.js
@@ -4,6 +4,16 @@ SecaoController.$inject = ['ApiFactory'];
 
 function SecaoController(ApiFactory) {
     const vm = this;
+    vm.secoesList = [];
 
+
+    if (ApiFactory.secoesList.length) {
+        vm.secoesList = ApiFactory.secoesList;
+    } else {
+        ApiFactory.getAllSecoes()
+            .then(resp => {
+                vm.secoesList = resp;
+            })
+    }
 
 }
